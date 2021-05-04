e74d752b7da12b015b70aa664de0595e7cf90860
MattiaFanan <mattia.fanan@gmail.com>
2021-05-04 15:39:23 +0200

    square the object

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,9 @@
 #include <opencv2/core.hpp>
 #include <opencv2/xfeatures2d.hpp>
 #include <opencv2/features2d.hpp>
-#include <opencv2/highgui.hpp>
+#include "opencv2/calib3d.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
 
 using namespace cv;
 using namespace std;
@@ -12,26 +14,20 @@ int main(int argc, char *argv[]) {
     String obj_folder = "/objects";
     String obj_file[] = {"/obj1.png", "/obj2.png", "/obj3.png", "/obj4.png"};
     //cout << path + obj_folder + obj_file[0];
-    Mat obj1 = imread(path + obj_folder + obj_file[0]);
+    Mat obj_image = imread(path + obj_folder + obj_file[0]);
 
     Ptr<SIFT> siftPtr = SIFT::create();
     BFMatcher matcher(cv::NORM_L2,true);
 
-    vector<KeyPoint> keypoints;
+    vector<KeyPoint> obj_keypoints;
     Mat obj_descriptor;
-    siftPtr->detectAndCompute(obj1, noArray(), keypoints, obj_descriptor);
-
-    // Add results to image.
-    Mat output;
-    drawKeypoints(obj1, keypoints, output);
-
+    siftPtr->detectAndCompute(obj_image, noArray(), obj_keypoints, obj_descriptor);
 
     VideoCapture cap(path+"/video.mov");
     if(!cap.isOpened()){
         cout << "Error opening video stream or file" << endl;
         return -1;
     }
-
     namedWindow("Frame", WINDOW_NORMAL);
 
     int cnt=0;
@@ -41,16 +37,15 @@ int main(int argc, char *argv[]) {
         if (frame.empty())
             break;
 
-        Mat imageMatches;
         if (cnt==0) {
 
             vector<KeyPoint> frame_keypoints;
             Mat frame_descriptor;
 
-            //compute keypoints for frame
+            //compute obj_keypoints for frame
             siftPtr->detectAndCompute(frame, noArray(), frame_keypoints, frame_descriptor);
 
-            //matches the keypoints
+            //matches the obj_keypoints
             vector<DMatch> matches;
             matcher.match(frame_descriptor, obj_descriptor, matches);
 
@@ -59,11 +54,51 @@ int main(int argc, char *argv[]) {
             for (DMatch match : matches)
                 kp_match.push_back(frame_keypoints.at(match.queryIdx));
 
+            //-- Filter matches using the Lowe's ratio test
+            /*
+            const float ratio_thresh = 0.75f;
+            std::vector<DMatch> good_matches;
+            for (size_t i = 0; i < matches.size(); i++)
+            {
+                if (matches[i][0].distance < ratio_thresh * matches[i][1].distance)
+                {
+                    good_matches.push_back(knn_matches[i][0]);
+                }
+            }*/
+
+            //RSAC
+            vector<Point2f> obj_points;
+            vector<Point2f> frame_points;
+            // get centers of the keypoints
+            for( size_t i = 0; i < matches.size(); i++ )
+            {
+                obj_points.push_back(obj_keypoints[ matches[i].trainIdx ].pt );
+                frame_points.push_back( frame_keypoints[ matches[i].queryIdx ].pt );
+            }
+            Mat H = findHomography( obj_points, frame_points, RANSAC);
+
+            //-- re-project the corners of the train object into the frame with the found homography
+            std::vector<Point2f> obj_corners(4);
+            obj_corners[0] = Point2f(0, 0);
+            obj_corners[1] = Point2f( (float)obj_image.cols, 0 );
+            obj_corners[2] = Point2f( (float)obj_image.cols, (float)obj_image.rows );
+            obj_corners[3] = Point2f( 0, (float)obj_image.rows );
+
+            std::vector<Point2f> scene_corners(4);
+            perspectiveTransform( obj_corners, scene_corners, H);
+
+            //-- Draw lines between the corners (the mapped object in the scene - image_2 )
+            line( frame, scene_corners[0],scene_corners[1], Scalar(0, 255, 0), 4 );
+            line( frame, scene_corners[1],scene_corners[2], Scalar( 0, 255, 0), 4 );
+            line( frame, scene_corners[2],scene_corners[3], Scalar( 0, 255, 0), 4 );
+            line( frame, scene_corners[3],scene_corners[0], Scalar( 0, 255, 0), 4 );
+
             //draw matches on frame
-            drawKeypoints(frame, kp_match, frame, Scalar(255,0,0));
+            //drawKeypoints(frame, kp_match, frame, Scalar(255,0,0));
+            imshow( "Frame", frame );
         }
 
-        imshow( "Frame", frame );
+        //imshow( "Frame", frame );
         cnt++;
 
         char c=(char)waitKey(25);
